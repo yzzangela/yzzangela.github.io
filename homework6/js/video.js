9698bdf56f74687210ec8ee7882cabd8fe68404d
yzzangela <70892261+yzzangela@users.noreply.github.com>
2020-11-22 17:07:29 -0800

    Update video.js

--- homework6/js/video.js
+++ homework6/js/video.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	let vol = document.querySelector("#volume").innerHTML = video.volume + "%";
+	let vol = document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
